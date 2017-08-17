7a7cf32db121f630668d6317b03b2d28e730b7b0
JoshuaStanley308 <joshua.stanley@my.jcu.edu.au>
2017-08-17 11:05:30 +1000

    update name thing file

--- prac_3/name_thing.py
+++ prac_3/name_thing.py
@@ -1,8 +1,22 @@
 
-print("Please enter your name: ")
-name = input("> ")
-while len(name) == 0:
-    print("error, cannot leave it blank")
+def main():
     print("Please enter your name: ")
     name = input("> ")
-print(name[0:len(name):2])
+    frequency_of_letters = int(input("Frequency of letters: "))
+    name = get_name(name)
+    print_name(name, frequency_of_letters)
+
+
+def print_name(name, frequency_of_letters):
+    length_of_name = len(name)
+    print(name[0:length_of_name:frequency_of_letters])
+
+
+def get_name(name):
+    while len(name) == 0:
+        print("error, cannot leave it blank")
+        print("Please enter your name: ")
+        name = input("> ")
+    return name
+
+main()
