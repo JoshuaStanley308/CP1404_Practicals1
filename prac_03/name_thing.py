1065a5ce8fdc4524f2cb1bf1bad118ed5db221bb
JoshuaStanley308 <joshua.stanley@my.jcu.edu.au>
2017-08-17 12:02:03 +1000

    updating formatting

--- prac_03/name_thing.py
+++ prac_03/name_thing.py
@@ -1,22 +1,21 @@
 
 def main():
-    print("Please enter your name: ")
-    name = input("> ")
+
+    name = get_name()
     frequency_of_letters = int(input("Frequency of letters: "))
-    name = get_name(name)
+
     print_name(name, frequency_of_letters)
 
 
 def print_name(name, frequency_of_letters):
-    length_of_name = len(name)
-    print(name[0:length_of_name:frequency_of_letters])
+    print(name[::frequency_of_letters])
 
 
-def get_name(name):
+def get_name():
+    name = input("Please enter your name: ")
     while len(name) == 0:
         print("error, cannot leave it blank")
-        print("Please enter your name: ")
-        name = input("> ")
+        name = input("Please enter your name: ")
     return name
 
 main()
