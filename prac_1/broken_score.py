1ffa5fed7cc3bc9923f375e2e5270911089b9d0b
JoshuaStanley308 <joshua.stanley@my.jcu.edu.au>
2017-08-31 10:22:44 +1000

    Adding prac_01

---
+++ prac_1/broken_score.py
@@ -0,0 +1,11 @@
+
+
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invlaid score")
+elif score < 50:
+    print("Bad")
+elif score >= 90:
+    print("Excellent")
+else:
+    print("Passable")
